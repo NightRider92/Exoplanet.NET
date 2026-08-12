ed789fadd28c8f305bc2b3f665d1bfeef6fdee4e
NightRider92 <moran.rod@gmail.com>
2026-08-12 08:57:21 +0200

    Add files via upload

---
+++ Exoplanet/PlotService/IPlotService.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExoPlanet.NET.Exoplanet.PlotService
+{
+    /// <summary>
+    /// Contains methods for plotting exoplanet data.
+    /// </summary>
+    public interface IPlotService
+    {
+        public Task<PlotModel> CreateComparisonGraph(string graphName, float xValue, float yValue, string xAxisTitle, string yAxisTitle, (float min, float max)? shadedBandY = null, string? shadedBandTitle = null);
+    }
+}
