a5096780a1f61c3b7dae2362585a0eadd5a362bb
NightRider92 <moran.rod@gmail.com>
2026-08-12 08:56:12 +0200

    Add files via upload

---
+++ Exoplanet/IExoplanet.cs
@@ -0,0 +1,16 @@
+﻿using ExoPlanet.NET.Exoplanet.Data;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExoPlanet.NET.Exoplanet
+{
+    /// <summary>
+    /// Interface for exoplanet data retrieval.
+    /// </summary>
+    public interface IExoplanet
+    {
+        public Task<ConcurrentBag<ExoplanetProperties?>?> GetDataAsync();
+    }
+}
